eb30eabf1a3b9df2764d3d8762bf56d4a2cd328e
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-11-22 11:39:47 +0100

    Adding bootstrap

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -8,6 +8,7 @@
 //= require jquery-2.1.3.js
 //= require_tree .
 //= require_self
+//= require bootstrap
 
 if (typeof jQuery !== 'undefined') {
     (function($) {
--- grails-app/views/backgroundJobs/index.gsp
+++ grails-app/views/backgroundJobs/index.gsp
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
     <head>
+        <r:require modules="bootstrap"/>
         <title>List of jobs</title>
     </head>
     <body>
